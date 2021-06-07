afceb02c03121292f4b6adbb41041955a9b154cb
Jasna Polak <jasna.polak@gmail.com>
2021-06-07 16:15:26 +0200

    initial commit

---
+++ casino.py
@@ -0,0 +1,10 @@
+secret = 3
+
+guess = int(input("Guess the secret number: "))
+
+if secret == guess:
+    print("Congratulations, you won $1,000 !!!")
+
+else:
+    print("Your guess is wrong. More luck next time!")
+
