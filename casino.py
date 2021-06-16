1abaa2b92c8a9cdabd19e9c696f26defca754c81
jasnapolak <77551420+jasnapolak@users.noreply.github.com>
2021-06-16 19:10:20 +0200

    Add files via upload

--- casino.py
+++ casino.py
@@ -1,10 +1,34 @@
-secret = 3
+import random
+import json
+import datetime
 
-guess = int(input("Guess the secret number: "))
+secret = random.randint(1,10)
+attempts = 0
 
-if secret == guess:
-    print("Congratulations, you won $1,000 !!!")
+player = input("Hello, What is your name? ")
 
-else:
-    print("Your guess is wrong. More luck next time!")
+with open("score_list.json", "r") as score_file:
+    score_list = json.loads(score_file.read())
+
+for score_dict in score_list:
+    print (str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+
+while True:
+    guess = int(input("Guess the secret number: (between 1 and 10): "))
+    attempts += 1
+
+    if secret == guess:
+        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})
+
+        with open("score_list.json", "w") as score_file:
+            score_file.write(json.dumps(score_list))
+
+        print("Congratulations, you won $1,000 !!!")
+        print("Attempts needed: " +str(attempts))
+        break
+    elif guess < secret:
+        print("Your guess is too small.")
+    elif guess > secret:
+        print("Your guess is too big.")
+
 
