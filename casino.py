2f83226ace4795648feacffa5ed6bce2695c7668
jasnapolak <77551420+jasnapolak@users.noreply.github.com>
2021-06-19 14:41:46 +0200

    Add files via upload

--- casino.py
+++ casino.py
@@ -14,7 +14,7 @@ score_list.sort(key=operator.itemgetter("attempts"))
 
 for score_dict in score_list[:3]:
     score_text = "Player {0} had {1} attempts on {2}. The secret number was {3}. The wrong guesses were: {4}"\
-                 .format(score_dict.get("player_name"),
+                 .format(score_dict.get("player"),
                         str(score_dict.get("attempts")),
                         score_dict.get("date"),
                         score_dict.get("secret_number"),
@@ -29,7 +29,7 @@ while True:
     attempts += 1
 
     if secret == guess:
-        score_list.append({"attempts": attempts, "wrong guesses": wrong_guesses, "secret_number": secret,
+        score_list.append({"attempts": attempts, "wrong_guesses": wrong_guesses, "secret_number": secret,
                            "player": player, "date": str(datetime.datetime.now())})
 
         with open("score_list.json", "w") as score_file:
